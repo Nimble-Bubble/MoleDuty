99fc5cdeb59f3548b974972c3e00097281f7da0b
Nimble-Bubble <44932098+Nimble-Bubble@users.noreply.github.com>
2024-04-28 20:56:43 -0400

    Test Character now moves normally

--- Areas/MiScellanea/TestCharacter.gd
+++ Areas/MiScellanea/TestCharacter.gd
@@ -5,10 +5,14 @@ extends CharacterBody2D
 @export var defaultAcceleration = 10;
 @export var jumpsLeft = 1;
 
+func _init():
+	velocity.y = 10;
 func _physics_process(delta):
 	var _vel = Vector2()
-	var grav = 150
-	
+	var grav = 490
+	slide_on_ceiling = false;
+	floor_stop_on_slope = false;
+	#if !is_on_floor:
 	velocity.y += delta * grav;
 	if is_on_floor:
 		#if (velocity.y < 0):
@@ -22,19 +26,28 @@ func _physics_process(delta):
 		velocity.x += 25;
 	if (velocity.x <= -500) or (velocity.x >= 500):
 		velocity.x *= 0.95;
+	if Input.is_action_just_pressed("p1dash"):
+		if velocity.x < 0:
+			velocity.x = -1000;
+		else:
+			velocity.x = 1000
+	velocity.x *= 0.9;
 	if Input.is_action_just_pressed("p1jump") and jumpsLeft > 0:
 
 		$Jumpsound.play();
-		velocity.y -= 200;
+		if (velocity.y > 0):
+			velocity.y = 0;
+		velocity.y -= 300;
 		jumpsLeft -= 1;
 	var motion = velocity * delta
-	move_and_collide(motion)
+	#motion = move_and_collide(motion)
+	move_and_slide();
 	frame_selector();
 func frame_selector():
 	if velocity.y != 0:
 		$AnimatedSprite2D.animation = "jump"
 		$AnimatedSprite2D.flip_v = false
-	if velocity.x != 0:
+	if velocity.x != 0 and Input.is_action_pressed("p1right") or velocity.x != 0 and Input.is_action_pressed("p1left"):
 		$AnimatedSprite2D.animation = "walk"
 		$AnimatedSprite2D.flip_v = false
 		$AnimatedSprite2D.flip_h = velocity.x < 0
