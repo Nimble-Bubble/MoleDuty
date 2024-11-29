3a74e016c46d6db1a4b649bf40ffe459215d0712
Nimble-Bubble <44932098+Nimble-Bubble@users.noreply.github.com>
2024-11-29 00:43:43 -0500

    Added further modifications to Testchar's movement code

--- Characters/Playable/Tester/TestCharacter.gd
+++ Characters/Playable/Tester/TestCharacter.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 @onready var rid = get_rid()
-@export var defaultSpeed = 40 * (10 / 9);
+@export var defaultSpeed = 40 * (10 / 8);
 @export var defaultAcceleration = 10;
 @export var jumpsLeft = 1;
 @export var bonusJumps = 0;
@@ -28,6 +28,7 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("togglevelocitylabel"):
 		$VelXLabel.visible = true;
 		$VelYLabel.visible = true;
+		$DashLabel.visible = true;
 	if Input.is_action_just_pressed("toggledash"):
 		$DashUnlock.play();
 		if canDash == 1:
@@ -70,8 +71,12 @@ func _physics_process(delta):
 		velocity.x -= defaultSpeed;
 	if Input.is_action_pressed("p1right"):
 		velocity.x += defaultSpeed;
-	if (velocity.x <= -500) or (velocity.x >= 500):
+	if (velocity.x <= -300) or (velocity.x >= 300):
 		velocity.x /= 1.01;
+		if (velocity.x <= -400) or (velocity.x >= 400):
+			velocity.x /= 1.02;
+			if (velocity.x <= -500) or (velocity.x >= 500):
+				velocity.x /= 1.03;
 	if canDash >= 1:
 		if dashTimer > 0:
 			dashTimer -= 1;
@@ -81,14 +86,14 @@ func _physics_process(delta):
 			#$DashOKIndicator.hidden = true;
 		if dashTimer == 50:
 			$Airjumpsound.play;
-		if dashTimer > 120:
+		if dashTimer > 135:
 			velocity.y = 0;
 		if Input.is_action_just_pressed("p1dash"):
 			if dashTimer < 50:
 				if velocity.x < 0:
-					velocity.x = -1000;
+					velocity.x = -2000;
 				else:
-					velocity.x = 1000;
+					velocity.x = 2000;
 				$Dashsound.play();
 				dashTimer += 150;
 				#if velocity.y > 0:
@@ -99,14 +104,14 @@ func _physics_process(delta):
 			velocity.x /= 1.01;
 		else:
 			velocity.x /= 1.005;
-	if Input.is_action_just_pressed("p1dash") and canDash >= 1:
-		if velocity.x < 0:
-			velocity.x = -1000;
-		else:
-			velocity.x = 1000;
-		$Dashsound.play();
-		if velocity.y > 0:
-			velocity.y = 0;
+	#if Input.is_action_just_pressed("p1dash") and canDash >= 1:
+		#if velocity.x < 0:
+			#velocity.x = -2000;
+		#else:
+			#velocity.x = 2000;
+		#$Dashsound.play();
+		#if velocity.y > 0:
+			#velocity.y = 0;
 	
 	if is_on_floor():
 		velocity.x *= 0.9;
@@ -120,9 +125,9 @@ func _physics_process(delta):
 			$Airjumpsound.play();
 		if is_on_wall() and canClimb == 1:
 			if velocity.x < 0:
-				velocity.x -= 400;
+				velocity.x -= 300;
 			else:
-				velocity.x += 400;
+				velocity.x += 300;
 		
 		if (velocity.y > 0 and !Input.is_action_pressed("p1down")):
 			velocity.y = 0;
